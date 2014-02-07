2ffef6b268b697f9a1967e362bad162f49506a91
danilo <danilo.adventista@gmail.com>
2014-02-07 14:20:06 -0200

    Criacao de controller para favoritos

---
+++ MPL/MPL/DMVTableVController.h
@@ -0,0 +1,15 @@
+//
+//  DMVViewController.h
+//  MPL
+//
+//  Created by VIHP on 2/5/14.
+//  Copyright (c) 2014 MPL Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
+
+@interface DMVTableVController : UIViewController <UITextFieldDelegate>
+
+@end
---
+++ MPL/MPL/DMVTableVController.m
@@ -0,0 +1,63 @@
+//
+//  DMVViewController.m
+//  MPL
+//
+//  Created by VIHP on 2/5/14.
+//  Copyright (c) 2014 MPL Foundation. All rights reserved.
+//
+
+#import "DMVTableVController.h"
+#import <MapKit/MapKit.h>
+#pragma mark
+
+@implementation DMVTableVController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+//  Inicia Toque longo.
+    
+//    Traz mapa \/
+}
+
+-(void) longPressGestureHandle:(UILongPressGestureRecognizer*)sender{
+    if(sender.state==UIGestureRecognizerStateEnded){
+        
+    }
+}
+
+-(void) locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+//    [super didReceiveMemoryWarning];
+    
+}
+
+//- (IBAction)btmSaida:(id)sender {
+//    [self findLocation:_tfSaida];
+//}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    return true;
+}
+
+
+-(NSString*)data{
+    
+    NSDate * now = [NSDate date];
+    NSDateFormatter *outputFormatter = [[NSDateFormatter alloc] init];
+    [outputFormatter setDateFormat:@"dd/MM/yy"];
+    NSString *newDateString = [outputFormatter stringFromDate:now];
+    
+    return newDateString;
+    
+}
+
+
+@end
